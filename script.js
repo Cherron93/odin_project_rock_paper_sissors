24b6f8f03f0c4884c5f697792e03001051b9d7ec
Cherron93 <colinherron93@gmail.com>
2022-05-04 06:45:54 -0400

    Replace console.log with innerText

--- script.js
+++ script.js
@@ -1,53 +1,87 @@
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+const roundWinner = document.querySelector('#roundWinner')
+const playerScoreDiv = document.querySelector('#playerScore')
+const computerScoreDiv = document.querySelector('#computerScore')
 const rockPaperSissors = ['rock', 'paper', 'sissors'];
 let playerScore = 0;
 let computerScore = 0;
 
-function computerPlay () {
+function computerSelection () {
     let randNum = (Math.floor(Math.random() * 3));
     return rockPaperSissors[randNum];
+    
 }
 
-// let computerSelection = computerPlay();
-
 function playRound (playerSelection, computerSelection) {
-    let newPlayerSelection = playerSelection.toLowerCase();
-    if (newPlayerSelection === 'rock' && computerSelection === 'paper') {
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
         computerScore ++;
-        console.log(`You Lose! Paper beats Rock. Player score = ${playerScore}. Computer score = ${computerScore}.`)
-    } else if (newPlayerSelection === 'paper' && computerSelection === 'rock') {
+        computerScore.innerText = `Computer Score is ${computerScore}`;
+        roundWinner.innerText = `You Lose! Paper beats Rock. Player score = ${playerScore}. Computer score = ${computerScore}.`
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         playerScore ++;
-        console.log(`You win! Paper beats Rock. Player score = ${playerScore}. Computer score = ${computerScore}.`)
-    } else if (newPlayerSelection === 'paper' && computerSelection === 'sissors') {
+        roundWinner.innerText = `You win! Paper beats Rock. Player score = ${playerScore}. Computer score = ${computerScore}.`
+    } else if (playerSelection === 'paper' && computerSelection === 'sissors') {
         computerScore ++;
-        console.log(`You lose! Sissors beats Paper. Player score = ${playerScore}. Computer score = ${computerScore}.`)
-    } else if (newPlayerSelection === 'sissors' && computerSelection === 'paper') {
+        roundWinner.innerText = `You lose! Sissors beats Paper. Player score = ${playerScore}. Computer score = ${computerScore}.`
+    } else if (playerSelection === 'sissors' && computerSelection === 'paper') {
         playerScore ++;
-        console.log(`You win! Sissors beats Paper. Player score = ${playerScore}. Computer score = ${computerScore}.`)
-    } else if (newPlayerSelection === 'sissors' && computerSelection === 'rock') {
+        roundWinner.innerText = `You win! Sissors beats Paper. Player score = ${playerScore}. Computer score = ${computerScore}.`
+    } else if (playerSelection === 'sissors' && computerSelection === 'rock') {
         computerScore ++;
-        console.log(`You lose! Rock beats sissors. Player score = ${playerScore}. Computer score = ${computerScore}.`)
-    } else if (newPlayerSelection === 'rock' && computerSelection === 'sissors') {
+        roundWinner.innerText = `You lose! Rock beats sissors. Player score = ${playerScore}. Computer score = ${computerScore}.`
+    } else if (playerSelection === 'rock' && computerSelection === 'sissors') {
         playerScore ++;
-        console.log(`You win! Rock beats sissors. Player score = ${playerScore}. Computer score = ${computerScore}.`)
-    } else if (newPlayerSelection === computerSelection) {
-        console.log(`Draw! Player score = ${playerScore}. Computer score = ${computerScore}.`)
+        roundWinner.innerText = `You win! Rock beats sissors. Player score = ${playerScore}. Computer score = ${computerScore}.`
+    } else if (playerSelection === computerSelection) {
+        roundWinner.innerText = `Draw! Player score = ${playerScore}. Computer score = ${computerScore}.`
     } else {
-        console.log('Not a valid input. Try entering rock, paper, or sissors')
+        roundWinner.innerText = 'Not a valid input. Try entering rock, paper, or sissors'
     }
 }; 
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = window.prompt('Enter Rock, Paper, or Scissors');
-        let computerSelection = computerPlay();
-        playRound (playerSelection, computerSelection); 
-    }
-    if (playerScore > computerScore) {
-        console.log(`Congrats! You win! Final player score = ${playerScore}. Final computer score = ${computerScore}.`)
-    } else if (computerScore > playerScore) {
-        console.log(`Boo! You lose! Final player score = ${playerScore}. Final computer score = ${computerScore}.`)
-    } else if (computerScore === playerScore) {
-        console.log(`By golly, we have a draw! Final player score = ${playerScore}. Final computer score = ${computerScore}.`)
-    }
-}
+rockButton.addEventListener('click', function run() {
+    playerSelection = 'rock'
+    playRound();
+    console.log(playerSelection);
+    console.log(computerSelection());
+});
+
+
+paperButton.addEventListener('click', function run() {
+    playerSelection = 'paper'
+    playRound();
+    console.log(playerSelection);
+    console.log(computerSelection());
+});
+
+scissorsButton.addEventListener('click', function run() {
+    playerSelection = 'paper'
+    playRound();
+    console.log(playerSelection);
+    console.log(computerSelection());
+});
+
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         let playerSelection = window.prompt('Enter Rock, Paper, or Scissors');
+//         let computerSelection = computerPlay();
+//         playRound (playerSelection, computerSelection); 
+//     }
+//     if (playerScore > computerScore) {
+//         console.log(`Congrats! You win! Final player score = ${playerScore}. Final computer score = ${computerScore}.`)
+//     } else if (computerScore > playerScore) {
+//         console.log(`Boo! You lose! Final player score = ${playerScore}. Final computer score = ${computerScore}.`)
+//     } else if (computerScore === playerScore) {
+//         console.log(`By golly, we have a draw! Final player score = ${playerScore}. Final computer score = ${computerScore}.`)
+//     }
+// }
+
+// buttons.addEventListener('click', computerPlay(), playRound());
+
+
+
+
+
 
